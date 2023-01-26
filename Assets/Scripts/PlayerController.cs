3af1493e73409415531641191069d2d655e7c399
Xiaoen <xikuang5@s.sfusd.edu>
2023-01-26 12:43:17 -0800

    copied files over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Lauch a projectile from a player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 
--- README.md
+++
@@ -1,2 +0,0 @@
-# Prototype-2
- Create with Code Unit 2
